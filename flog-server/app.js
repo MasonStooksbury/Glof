3db2989032e106fce20ba0cd20670246eed8b66f
Mason Stooksbury <mestooksbu42@students.tntech.edu>
2020-04-05 16:16:18 -0400

    Added very simple example of multi-client-single-server setup. Works!

---
+++ flog-server/app.js
@@ -0,0 +1,40 @@
+const Express = require("express")();
+const Http = require("http").Server(Express);
+const Socketio = require("socket.io")(Http);
+
+Http.listen(3000, () => {
+    console.log("Listening at :3000...");
+});
+
+var position = {
+    x: 200,
+    y: 200
+};
+
+Socketio.on("connection", socket => {
+    socket.emit("position", position);
+    socket.on("move", data => {
+        switch(data) {
+            case "left":
+                position.x -= 10;
+                Socketio.emit("position", position);
+                break;
+            case "right":
+                position.x += 10;
+                Socketio.emit("position", position);
+                break;
+            case "up":
+                position.y -= 10;
+                Socketio.emit("position", position);
+                break;
+            case "down":
+                position.y += 10;
+                Socketio.emit("position", position);
+                break;
+        }
+    });
+});
+
+// Key difference between socket.emit() and Socketio.emit():
+//      - socket.emit()     = Only one socket will receive the message
+//      - Socketio.emit()   = ALL connected sockets will receive the message
