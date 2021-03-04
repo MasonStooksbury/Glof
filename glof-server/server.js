ffabd18f78ea640c8545dae9834455f05f38e601
Mason Stooksbury <40926080+MasonStooksbury@users.noreply.github.com>
2021-03-03 22:55:00 -0600

    Update server.js

--- glof-server/server.js
+++ glof-server/server.js
@@ -5,8 +5,8 @@ const Http = require('http').Server(Express);
 const io = require('socket.io')(Http);
 const cors = require('cors');
 
-Http.listen(709, () => {
-    console.log('Listening at :709...');
+Http.listen(24698, () => {
+    console.log('Listening at :24698...');
 });
 
 Express.use(cors());
